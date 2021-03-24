510d7ce3cbdc72f550f61477394135b412edd371
Szabó György <geoedevices@live.com>
2021-03-24 16:12:07 +0100

    Finished project

--- src/main/java/com/codecool/chessopen/ChessResults.java
+++ src/main/java/com/codecool/chessopen/ChessResults.java
@@ -1,20 +1,50 @@
 package com.codecool.chessopen;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.*;
+import java.util.*;
 
 public class ChessResults {
 
+    private class Competitor {
+
+        private final String NAME;
+        private final int TOTAL_POINTS;
+
+        private Competitor(String name, int totalPoints) {
+            this.NAME = name;
+            this.TOTAL_POINTS = totalPoints;
+        }
+
+    }
+
     public List<String> getCompetitorsNamesFromFile(String fileName){
-        //TODO your code comes here
-        return null;
+        List<Competitor> competitors = new ArrayList<>();
+        try (Scanner scanner = new Scanner(new File(fileName))) {
+            while (scanner.hasNextLine()) {
+                String[] lineContent = scanner.nextLine().split(",");
+                int totalPoints = Integer.parseInt(lineContent[1]) +
+                        Integer.parseInt(lineContent[2]) +
+                        Integer.parseInt(lineContent[3]) +
+                        Integer.parseInt(lineContent[4]) +
+                        Integer.parseInt(lineContent[5]);
+                competitors.add(new Competitor(lineContent[0], totalPoints));
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+        }
+        List<String> listToReturn = new ArrayList<>();
+        Competitor dummyCompetitor = new Competitor("", -1);
+        while (competitors.size() > 0) {
+            Competitor highestPointsCompetitor = dummyCompetitor;
+            for (Competitor actualCompetitor : competitors) {
+                if (actualCompetitor.TOTAL_POINTS > highestPointsCompetitor.TOTAL_POINTS) {
+                    highestPointsCompetitor = actualCompetitor;
+                }
+            }
+            listToReturn.add(highestPointsCompetitor.NAME);
+            competitors.remove(highestPointsCompetitor);
+        }
+        return listToReturn;
     }
 
 }
